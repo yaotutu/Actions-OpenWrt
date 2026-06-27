94717e81a0c1e5b7a0078abc0116602392146b7d
Yao Tutu <957810668@qq.com>
2026-06-27 17:12:41 +0800

    Revise custom-config.sh with new firmware settings
    Updated custom-config.sh to include new configurations for GL.iNet GL-MT3000 and additional packages like daed and sing-box. Removed obsolete configurations and added comments for clarity.

--- custom-config.sh
+++ custom-config.sh
@@ -1,45 +1,48 @@
-# Use "make defconfig" to generate a complete .config file
+#!/bin/bash
 
-# Custom firmware base
-# echo 'CONFIG_TARGET_x86=y' >> $BUILD_ROOT/.config
-# echo 'CONFIG_TARGET_x86_64=y' >> $BUILD_ROOT/.config
-# echo 'CONFIG_TARGET_x86_64_DEVICE_generic=y' >> $BUILD_ROOT/.config
-# echo 'CONFIG_TARGET_ROOTFS_PARTSIZE=3072' >> $BUILD_ROOT/.config
-
+# GL.iNet GL-MT3000 / Beryl AX
 echo 'CONFIG_TARGET_mediatek=y' >> $BUILD_ROOT/.config
 echo 'CONFIG_TARGET_mediatek_filogic=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_TARGET_mediatek_filogic_DEVICE_bananapi_bpi-r3-mini=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_TARGET_ROOTFS_PARTSIZE=4096' >> $BUILD_ROOT/.config
+echo 'CONFIG_TARGET_mediatek_filogic_DEVICE_glinet_gl-mt3000=y' >> $BUILD_ROOT/.config
 
-# Lanuage Setting
-echo 'CONFIG_LUCI_LANG_zh_Hans=y' >> $BUILD_ROOT/.config
-
-# Users & Groups Support
-echo 'CONFIG_PACKAGE_shadow-utils=y' >> $BUILD_ROOT/.config
-
-# Disk Support
-echo 'CONFIG_PACKAGE_kmod-nvme=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_fdisk=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_block-mount=y' >> $BUILD_ROOT/.config
-
-# Add OpenClash
-echo '# CONFIG_PACKAGE_dnsmasq is not set' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_luci-compat=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_wget-ssl=y' >> $BUILD_ROOT/.config
-
-# Add Docker
-echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_dockerd=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_docker-compose=y' >> $BUILD_ROOT/.config
-
-# Add other apps
+# LuCI
 echo 'CONFIG_PACKAGE_luci=y' >> $BUILD_ROOT/.config
 echo 'CONFIG_PACKAGE_luci-ssl=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_LUCI_LANG_zh_Hans=y' >> $BUILD_ROOT/.config
 
-echo 'CONFIG_PACKAGE_luci-app-wol=y' >> $BUILD_ROOT/.config
+# daed
+echo 'CONFIG_PACKAGE_daed=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_PACKAGE_luci-app-daed=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_PACKAGE_luci-compat=y' >> $BUILD_ROOT/.config
 
-echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> $BUILD_ROOT/.config
+# daed eBPF requirements
+echo 'CONFIG_DEVEL=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_KERNEL_DEBUG_INFO=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_KERNEL_DEBUG_INFO_REDUCED=n' >> $BUILD_ROOT/.config
+echo 'CONFIG_KERNEL_DEBUG_INFO_BTF=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_KERNEL_CGROUPS=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_KERNEL_CGROUP_BPF=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_KERNEL_BPF_EVENTS=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_BPF_TOOLCHAIN_HOST=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_KERNEL_XDP_SOCKETS=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_PACKAGE_kmod-xdp-sockets-diag=y' >> $BUILD_ROOT/.config
 
-echo 'CONFIG_PACKAGE_luci-app-samba4=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_PACKAGE_luci-app-upnp=y' >> $BUILD_ROOT/.config
+# sing-box
+echo 'CONFIG_PACKAGE_sing-box=y' >> $BUILD_ROOT/.config
+echo '# CONFIG_SING_BOX_BUILD_CLASH_API is not set' >> $BUILD_ROOT/.config
+echo '# CONFIG_SING_BOX_BUILD_TAILSCALE is not set' >> $BUILD_ROOT/.config
+echo '# CONFIG_SING_BOX_BUILD_ACME is not set' >> $BUILD_ROOT/.config
+echo '# CONFIG_SING_BOX_BUILD_DHCP is not set' >> $BUILD_ROOT/.config
+echo '# CONFIG_SING_BOX_BUILD_EMBEDDED_TOR is not set' >> $BUILD_ROOT/.config
+echo '# CONFIG_SING_BOX_BUILD_V2RAY_API is not set' >> $BUILD_ROOT/.config
+echo 'CONFIG_SING_BOX_BUILD_GVISOR=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_SING_BOX_BUILD_QUIC=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_SING_BOX_BUILD_UTLS=y' >> $BUILD_ROOT/.config
+
+# Explicitly avoid noisy proxy stacks
+echo '# CONFIG_PACKAGE_luci-app-openclash is not set' >> $BUILD_ROOT/.config
+echo '# CONFIG_PACKAGE_luci-app-passwall is not set' >> $BUILD_ROOT/.config
+echo '# CONFIG_PACKAGE_luci-app-passwall2 is not set' >> $BUILD_ROOT/.config
+echo '# CONFIG_PACKAGE_luci-app-ssr-plus is not set' >> $BUILD_ROOT/.config
+echo '# CONFIG_PACKAGE_mihomo is not set' >> $BUILD_ROOT/.config
+echo '# CONFIG_PACKAGE_clash is not set' >> $BUILD_ROOT/.config
