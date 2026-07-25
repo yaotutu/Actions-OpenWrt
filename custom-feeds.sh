f185be659746e38190c1f2fb8746a1b515f0b78d
yaotutu <957810668@qq.com>
2026-07-25 12:45:43 +0530

    build: fix passwall2 feed priority and upgrade golang to 1.25
    geoview 0.2.x requires Go >= 1.25 while openwrt-24.10 ships 1.23, so
    package/feeds/passwall_packages/geoview failed to build.
    
    - custom-feeds.sh: prepend the passwall feeds to feeds.conf.default so
      passwall_packages takes priority over the official packages feed for
      duplicate packages (sing-box, xray-core, v2ray-geodata, ...)
    - prebuild-misc.sh: replace feeds/packages/lang/golang with
      sbwml/packages_lang_golang (25.x branch) after feeds install

--- custom-feeds.sh
+++ custom-feeds.sh
@@ -3,5 +3,7 @@
 set -e
 
 # PassWall2: packages feed + LuCI app feed.
-echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >> "$BUILD_ROOT/feeds.conf.default"
-echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' >> "$BUILD_ROOT/feeds.conf.default"
+# Prepend (not append) so passwall_packages wins over the official packages
+# feed for duplicate packages (sing-box, xray-core, v2ray-geodata, ...).
+sed -i '1i src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' "$BUILD_ROOT/feeds.conf.default"
+sed -i '1i src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' "$BUILD_ROOT/feeds.conf.default"
--- prebuild-misc.sh
+++ prebuild-misc.sh
@@ -4,4 +4,14 @@ fix_rust_compile_error() {
     fi
 }
 
+# openwrt-24.10 ships Go 1.23, but geoview 0.2.x and recent sing-box/xray
+# require Go >= 1.25. Replace the feed's golang with a newer toolchain.
+upgrade_golang() {
+    if [ -d "$BUILD_ROOT/feeds/packages/lang/golang" ]; then
+        rm -rf "$BUILD_ROOT/feeds/packages/lang/golang"
+        git clone --depth 1 -b 25.x https://github.com/sbwml/packages_lang_golang.git "$BUILD_ROOT/feeds/packages/lang/golang"
+    fi
+}
+
 fix_rust_compile_error
+upgrade_golang
